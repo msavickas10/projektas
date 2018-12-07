96fb54bbef44af910942ed57bad5b6fdb284f014
msavickas10 <msavickas10@gmail.com>
2018-12-07 09:33:05 +0200

    gyivguh

--- src/main/java/org/cards/poker/holdem/cards/CardsDeck.java
+++ src/main/java/org/cards/poker/holdem/cards/CardsDeck.java
@@ -15,7 +15,33 @@ public class CardsDeck {
     }
 
     public void reset() {
+        cards.add(new Card(Rank.Rank_10, Suits.Diamonds));
         cards.add(new Card(Rank.Rank_2, Suits.Diamonds));
+        cards.add(new Card(Rank.Rank_3, Suits.Diamonds));
+        cards.add(new Card(Rank.Rank_4, Suits.Diamonds));
+        cards.add(new Card(Rank.Rank_5, Suits.Diamonds));
+        cards.add(new Card(Rank.Rank_6, Suits.Diamonds));
+        cards.add(new Card(Rank.Rank_7, Suits.Diamonds));
+        cards.add(new Card(Rank.Rank_8, Suits.Diamonds));
+        cards.add(new Card(Rank.Rank_9, Suits.Diamonds));
+        cards.add(new Card(Rank.Rank_Ace, Suits.Diamonds));
+        cards.add(new Card(Rank.Rank_King, Suits.Diamonds));
+        cards.add(new Card(Rank.Rank_Jack, Suits.Diamonds));
+        cards.add(new Card(Rank.Rank_Queen, Suits.Diamonds));
+        cards.add(new Card(Rank.Rank_10, Suits.Clubs));
+        cards.add(new Card(Rank.Rank_2, Suits.Clubs));
+        cards.add(new Card(Rank.Rank_3, Suits.Clubs);
+        cards.add(new Card(Rank.Rank_4, Suits.Clubs));
+        cards.add(new Card(Rank.Rank_5, Suits.Clubs));
+        cards.add(new Card(Rank.Rank_6, Suits.Clubs));
+        cards.add(new Card(Rank.Rank_7, Suits.Clubs));
+        cards.add(new Card(Rank.Rank_8, Suits.Clubs));
+        cards.add(new Card(Rank.Rank_9, Suits.Clubs));
+        cards.add(new Card(Rank.Rank_Ace, Suits.Clubs));
+        cards.add(new Card(Rank.Rank_King, Suits.Clubs));
+        cards.add(new Card(Rank.Rank_Jack, Suits.Clubs));
+        cards.add(new Card(Rank.Rank_Queen, Suits.Clubs));
+
         //TODO
     }
 
